24e30f1ad47db5b67ee9cae355477d78091a3b37
Dennis Meenhuis <github@dennus.net>
2021-01-09 09:21:41 +0100

    Updated custom components

--- custom_components/nefiteasy/nefit_entity.py
+++ custom_components/nefiteasy/nefit_entity.py
@@ -1,34 +1,43 @@
-import asyncio
-import concurrent
-import logging
+"""Support for Bosch home thermostats."""
 
-from homeassistant.helpers.dispatcher import async_dispatcher_send, async_dispatcher_connect
+import asyncio
+import logging
+from typing import Callable, Dict, List
+
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity import Entity
 
-from .const import DISPATCHER_ON_DEVICE_UPDATE, CONF_NAME, STATE_CONNECTION_VERIFIED
+from .const import (
+    CONF_NAME,
+    CONF_SERIAL,
+    DISPATCHER_ON_DEVICE_UPDATE,
+    DOMAIN,
+    STATE_CONNECTION_VERIFIED,
+)
 
 _LOGGER = logging.getLogger(__name__)
+
 
 class NefitEntity(Entity):
     """Representation of a Nefit entity."""
 
-    def __init__(self, device, key, typeconf):
+    def __init__(self, client, data, key, typeconf):
         """Initialize the sensor."""
-        self._client = device['client']
-        self._config = device['config']
+        self._client = client
+        self._config = data
         self._typeconf = typeconf
         self._key = key
-        self._unique_id = "%s_%s" % (self._client.nefit.serial_number, self._key)
+        self._unique_id = f"{self._client.nefit.serial_number}_{self._key}"
 
         self._client.events[key] = asyncio.Event()
         self._client.data[key] = None
 
-        if 'url' in typeconf:
-            self._url = typeconf['url']
+        if "url" in typeconf:
+            self._url = typeconf["url"]
             self._client.keys[self._url] = key
 
-        if 'short' in typeconf:
-            self._client.uiStatusVars[key] = typeconf['short']
+        if "short" in typeconf:
+            self._client.ui_status_vars[key] = typeconf["short"]
 
         self._remove_callbacks: List[Callable[[], None]] = []
 
@@ -39,7 +48,9 @@ class NefitEntity(Entity):
         }
         self._remove_callbacks.append(
             async_dispatcher_connect(
-                self.hass, DISPATCHER_ON_DEVICE_UPDATE.format(**kwargs), self.async_schedule_update_ha_state
+                self.hass,
+                DISPATCHER_ON_DEVICE_UPDATE.format(**kwargs),
+                self.async_schedule_update_ha_state,
             )
         )
 
@@ -48,26 +59,31 @@ class NefitEntity(Entity):
         for remove_callback in self._remove_callbacks:
             remove_callback()
         self._remove_callbacks = []
-        
-        del self._client.events[self._key] 
-        del self._client.uiStatusVars[key]
+
+        del self._client.events[self._key]
+        if self._key in self._client.ui_status_vars:
+            del self._client.ui_status_vars[self._key]
 
     async def async_update(self):
         """Request latest data."""
         _LOGGER.debug("async_update called for %s", self._key)
         event = self._client.events[self._key]
-        event.clear() #clear old event
+        event.clear()  # clear old event
         self._client.nefit.get(self.get_endpoint())
         try:
             await asyncio.wait_for(event.wait(), timeout=9)
-        except concurrent.futures._base.TimeoutError:
-            _LOGGER.debug("Did not get an update in time for %s %s.", self._client.serial, self._key)
-            event.clear() #clear event
-        
+        except asyncio.TimeoutError:
+            _LOGGER.debug(
+                "Did not get an update in time for %s %s.",
+                self._client.serial,
+                self._key,
+            )
+            event.clear()  # clear event
+
     @property
     def name(self):
-        """Return the name of the device. """
-        return "%s %s" % (self._config[CONF_NAME], self._typeconf['name'])
+        """Return the name of the device."""
+        return f'{self._config[CONF_NAME]} {self._typeconf["name"]}'
 
     @property
     def unique_id(self) -> str:
@@ -76,21 +92,31 @@ class NefitEntity(Entity):
 
     @property
     def should_poll(self) -> bool:
+        """Return if entity should poll."""
         if self._client.connected_state == STATE_CONNECTION_VERIFIED:
-            """Enable polling if value does not exist in uiStatus"""
-            if 'short' in self._typeconf:
+            # Enable polling if value does not exist in uiStatus
+            if "short" in self._typeconf:
                 return False
-            else:
-                return True
-        else:
-            return False
+
+            return True
+
+        return False
 
     @property
     def icon(self):
         """Return possible sensor specific icon."""
-        if 'icon' in self._typeconf:
-            return self._typeconf['icon']
-        
+        if "icon" in self._typeconf:
+            return self._typeconf["icon"]
+
     def get_endpoint(self):
         """Return the API endpoint."""
         return self._url
+
+    @property
+    def device_info(self) -> Dict[str, any]:
+        """Return the device information."""
+        return {
+            "identifiers": {(DOMAIN, self._config[CONF_SERIAL])},
+            "name": self._config[CONF_NAME],
+            "manufacturer": "Bosch",
+        }
